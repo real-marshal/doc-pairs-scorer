dff1e71b6396c6d98754c2c62fa5f084605cf130
Real_Marshal <remarshal@protonmail.com>
2023-02-22 19:56:57 +0300

    fix: 🐛  broken pagination

--- src/assets/script.js
+++ src/assets/script.js
@@ -4,12 +4,15 @@ export const setActiveLink = (href) =>
   document.querySelector(`a[href="${href}"]`)?.classList.add('active')
 
 export const renderPagination = ({ currentPage, totalPages, paginationContainer, to }) => {
-  const maxPagesToShow = 10
+  const maxPagesToShow = 9
   const pageCount = Math.min(maxPagesToShow, totalPages)
+  const shift = Math.max(currentPage - Math.round(maxPagesToShow / 2), 0)
+  const initialPage = 1 + shift
+  const lastPage = Math.min(pageCount + shift, totalPages)
 
   const paginationButtons = []
 
-  for (let i = 1; i <= pageCount; i++) {
+  for (let i = initialPage; i <= lastPage; i++) {
     const link = document.createElement('a')
     link.href = `${to}?page=${i}`
     link.textContent = i.toString()
